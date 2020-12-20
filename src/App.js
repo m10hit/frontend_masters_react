1300ebbe8e8ead72d9cdef37f93cfbadc899fde3
m10hit <mohitchauhan10@gmail.com>
2020-12-20 13:02:58 +1100

    hooks

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Pet from './Pet';
+import SearchParams from './SearchParams';
 
 const App = () => {
-  //   return React.createElement("div", {}, [
-  //     React.createElement("h1", {}, "Adopt Me!"),
-  //     React.createElement(Pet, {
-  //       name: "Lune",
-  //       animal: "Dog",
-  //       breed: "Labrador",
-  //     }),
-  //     React.createElement(Pet, {
-  //       name: "Pepper",
-  //       animal: "Bird",
-  //       breed: "Cockatiel",
-  //     }),
-  //     React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed" }),
-  //   ]);
-
   return (
     <div>
-      <h1>Adopt Me!</h1>
-      <Pet name="Lune" animal="Dog" breed="Labrador" />
-      <Pet name="Pepper" animal="Bird" breed="Cockateil" />
-      <Pet name="Doink" animal="Cat" breed="Mixed" />
+      <h1 id="something-important">Adopt Me!</h1>
+      <SearchParams />
     </div>
   );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
