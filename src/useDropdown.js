cb7148c990ddf3839826f2daed28fef7d41a0e1c
m10hit <mohitchauhan10@gmail.com>
2020-12-21 00:37:49 +1100

    use effect

--- src/useDropdown.js
+++ src/useDropdown.js
@@ -3,21 +3,20 @@ import React,{useState} from 'react';
 const useDropdown = (label,defaultState,options) => {
     const [state,setState] = useState(defaultState);
     const id  = `use-dropdown-${label.replace(" ","").toLowerCase()}`;
-    console.log(id);
     const Dropdown = () => (
         <label htmlFor={id}>
             {label}
             <select 
                 id={id}
                 value={state}
-                onChange={(e) => setState(e.target.valule)}    
-                onBlur={(e) => setState(e.target.valule)}    
+                onChange={e => setState(e.target.value)}    
+                onBlur={e => setState(e.target.value)}    
                 disabled = {options.length === 0}
             >
             <option>All</option>
-            {options.map(item => {
+            {options.map(item => (
                 <option key={item} value={item}>{item}</option>
-            })}
+            ))}
 
             </select>
         </label>
