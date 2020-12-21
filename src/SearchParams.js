037bedf2a30cc657cb5063bfd7240cff5340c601
m10hit <mohitchauhan10@gmail.com>
2020-12-22 00:35:15 +1100

    add Results

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,28 +1,42 @@
-import React, { useState,useEffect } from 'react';
-import pet,{ ANIMALS } from '@frontendmasters/pet';
+import React, { useState, useEffect } from 'react';
+import pet, { ANIMALS } from '@frontendmasters/pet';
 import useDropdown from './useDropdown';
+import Results from './Results';
 
 const SearchParams = () => {
   const [location, setLocation] = useState('Seattle,WA');
   const [breeds, setBreeds] = useState([]);
-  const [animal,AnimalDropdown] = useDropdown("Animal","dog",ANIMALS)
-  const [breed,BreedDropdown,setBreed] = useDropdown("Breed","",breeds)
+  const [animal, AnimalDropdown] = useDropdown('Animal', 'dog', ANIMALS);
+  const [breed, BreedDropdown, setBreed] = useDropdown('Breed', '', breeds);
+  const [pets,setPets] = useState([]); 
 
-
+  async function requestPets(){
+    const {animals} = await pet.animals({
+      location,
+      breed,
+      type:animal
+    })
+    setPets(animals || [])
+  }
   useEffect(() => {
-      setBreeds([]);
-      setBreed("");
-      pet.breeds(animal).then(({breeds})=>{
-          const breedStrings = breeds.map(({name}) => name);
-          setBreeds(breedStrings)
-      },console.error)
-  },[animal,setBreeds,setBreed])
-
+    setBreeds([]);
+    setBreed('');
+    pet
+      .breeds(animal)
+      .then(({breeds}) => {
+        const breedStrings = breeds.map(({ name }) => name);
+        setBreeds(breedStrings);
+      })
+      .catch((err) => console.log(err));
+  }, [animal,setBreed,setBreeds]);
 
   return (
     <div className="search-params">
       <h1>{location}</h1>
-      <form>
+      <form onSubmit={(e) => {
+        e.preventDefault()
+        requestPets();
+      }}>
         <label htmlFor="location">
           Location
           <input
@@ -37,6 +51,7 @@ const SearchParams = () => {
 
         <button>Submit</button>
       </form>
+      <Results pets={pets}/>
     </div>
   );
 };
